0209d4be40632f65ca8481b41c73d32d10e0808f
Anurag Chanani <achanani@iitk.ac.in>
2017-12-13 20:45:14 +0530

    added friends count and new followers list

--- twitter_stats.py
+++ twitter_stats.py
@@ -14,29 +14,49 @@ api = tweepy.API(auth)
 
 
 def insert_user(username):
-    # Creating the database
-    con = sql.connect("database_JB.db")
-    cur = con.cursor()
-    count = 0
-    follower_temp = 0
-    status_temp = 0
-    while True:
-        count += 1
-        print('I am in Loop ' + str(count))
-        # Getting the information about the user.
-        user = api.get_user(username)
-        follower = user.followers_count
-        status = user.statuses_count
-        # Executing an SQL statement to send to the database.
-        #cur.execute("INSERT INTO twitter_stats (username, follower_count, status_count) VALUES (?, ?, ?)", (username, '123', '456'))
-        if (status_temp!=status or follower_temp!=follower):
-            cur.execute("INSERT INTO twitter_stats (username, time, follower_count, status_count) VALUES (?, ?, ?, ?)", (username, time.ctime(), follower, status))
-            con.commit()
-            follower_temp = follower
-            status_temp = status
+   # Creating the database
+   con = sql.connect("database.db")
+   cur = con.cursor()
+   count = 0
+   follower_temp = 0
+   status_temp = 0
+   friend_temp = 0
+   temp_follower_id = 'abcd'
 
-        time.sleep(60)
 
-    con.close()
-users = ['ShahrukhKhan', 'KatrinaKaif', 'SachinTendulkar', 'piyushbhatia993', 'SundarPichai']
-insert_user('justinbieber')
+   while True:
+       count += 1
+       print('I am in Loop ' + str(count))
+       # Getting the information about the user.
+       user = api.get_user(username)
+       friend = user.friends_count
+       status = user.statuses_count
+       follower = user.followers_count
+       follower_list_class = api.followers(username,count = 100)
+       new_follower_list = []
+
+       #adding new followers in a list
+       for xyz in follower_list_class:
+
+        if (xyz.screen_name!=temp_follower_id):
+          new_follower_list.append(xyz.screen_name)
+        else:
+          break
+
+       temp_follower_id = follower_list_class[0].screen_name
+
+       # Executing an SQL statement to send to the database.
+       #cur.execute("INSERT INTO twitter_stats (username, follower_count, status_count) VALUES (?, ?, ?)", (username, '123', '456'))
+       if (status_temp!=status or follower_temp!=follower or friend!=friend_temp):
+           cur.execute("INSERT INTO twitter_stats (username, time, following_count,follower_count, status_count,new_followers) VALUES (?, ?, ?, ?, ?, ?)", (username, time.ctime(),friend, follower, status, " ".join(new_follower_list)))
+           con.commit()
+           follower_temp = follower
+           status_temp = status
+           friend_temp = friend
+
+       time.sleep(60)
+
+   con.close()
+
+users = ['twitter', 'VENETHIS', 'katyperry', 'justinbieber', '6BillionPeople']
+insert_user('katyperry')
