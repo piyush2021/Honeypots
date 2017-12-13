6ff9a2c880ab27725fd814d1fd6a15970c278d6b
Anurag Chanani <achanani@iitk.ac.in>
2017-12-13 20:42:03 +0530

    added new followers column

--- schema.sql
+++ schema.sql
@@ -1,8 +1,11 @@
+
 drop table if exists twitter_stats;
 create table twitter_stats (
-id integer primary key autoincrement,
-time text not null,
-username text not null,
-follower_count text not null,
-status_count text not nulls
+	id integer primary key autoincrement,
+	time text not null,
+	username text not null,
+	following_count text not null,
+	follower_count text not null,
+	status_count text not null,
+	new_followers text not null
 );
